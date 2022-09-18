e29d340a323a310008c21bd0749ee02f51ddbff1
☠︎︎•PɪRᴀTᴇ•☠︎︎ <78387329+Pirate4Gamer@users.noreply.github.com>
2022-09-18 16:30:12 +0100

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.Prefix = `.`;
-exports.Token = `OTMOODE50TUzMDI5MjkyMDUy.Ye1o7A.6yNy-EHh1_KIquG3XZEh371GtPo`;
+exports.Token = `YOUR-BOTS-TOKEN`;
 exports.Color = `RANDOM`;
 exports.Footerequest = `Requested by ${message.author.username}`;
 exports.Footerpirate = `PiRaTe BoT`;
